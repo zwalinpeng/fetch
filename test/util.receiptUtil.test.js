0dff9143076fc217410f8179aa3dbfba85950f82
zoie peng <zoiepng3102@gmail.com>
2025-04-12 10:40:48 -0400

    update receiptUtil tests

--- test/util.receiptUtil.test.js
+++ test/util.receiptUtil.test.js
@@ -1,6 +1,102 @@
 const receiptUtil = require("../src/util/receiptUtil");
 
 describe("Test calculatePoints()", () => {
+    test("+1 for every alphanumeric character in retailer name", () => {
+        const receipt = {
+            retailer: "T4-get",
+            purchaseDate: "2022-03-20",
+            purchaseTime: "11:33",
+            items: [],
+            total: "9.99",
+        };
+        expect(receiptUtil.calculatePoints(receipt)).toBe(5);
+    });
+    test("+75 for round total", () => {
+        const receipt = {
+            retailer: "T4-get",
+            purchaseDate: "2022-03-20",
+            purchaseTime: "11:33",
+            items: [],
+            total: "9.00",
+        };
+        expect(receiptUtil.calculatePoints(receipt)).toBe(80);
+    });
+    test("+25 for total multiple of 0.25", () => {
+        const receipt = {
+            retailer: "T4-get",
+            purchaseDate: "2022-03-20",
+            purchaseTime: "11:33",
+            items: [],
+            total: "9.25",
+        };
+        expect(receiptUtil.calculatePoints(receipt)).toBe(30);
+    });
+    test("+5 for every 2 items", () => {
+        const receipt = {
+            retailer: "T4-get",
+            purchaseDate: "2022-03-20",
+            purchaseTime: "11:33",
+            items: [
+                {
+                    shortDescription: "Gatorade",
+                    price: "2.25",
+                },
+                {
+                    shortDescription: "Gatorade",
+                    price: "2.25",
+                },
+                {
+                    shortDescription: "Gatorade",
+                    price: "2.25",
+                },
+                {
+                    shortDescription: "Gatorade",
+                    price: "2.25",
+                },
+                {
+                    shortDescription: "Gatorade",
+                    price: "2.25",
+                },
+            ],
+            total: "9.99",
+        };
+        expect(receiptUtil.calculatePoints(receipt)).toBe(15);
+    });
+    test("+price * 0.2 rounded up if trimmed desc is multiple of 3", () => {
+        const receipt = {
+            retailer: "T4-get",
+            purchaseDate: "2022-03-20",
+            purchaseTime: "11:33",
+            items: [
+                {
+                    shortDescription: "   Klarbrunn 12-PK 12 FL OZ  ",
+                    price: "12.00",
+                },
+            ],
+            total: "9.99",
+        };
+        expect(receiptUtil.calculatePoints(receipt)).toBe(8);
+    });
+    test("+6 for odd date", () => {
+        const receipt = {
+            retailer: "T4-get",
+            purchaseDate: "2022-03-21",
+            purchaseTime: "11:33",
+            items: [],
+            total: "9.99",
+        };
+        expect(receiptUtil.calculatePoints(receipt)).toBe(11);
+    });
+    test("+10 for time between 2:00pm and 4:00pm", () => {
+        const receipt = {
+            retailer: "T4-get",
+            purchaseDate: "2022-03-20",
+            purchaseTime: "14:33",
+            items: [],
+            total: "9.99",
+        };
+        expect(receiptUtil.calculatePoints(receipt)).toBe(15);
+    });
     test("Example 1", () => {
         const receipt = {
             retailer: "Target",
@@ -121,11 +217,27 @@ describe("Test validateReceipt()", () => {
         expect(receiptUtil.validateReceipt(receipt)).toBe(false);
     });
 
-    test("Receipt with invalid time format should return false", () => {
+    test("Receipt with invalid time should return false", () => {
         const receipt = {
             retailer: "CVS",
             purchaseDate: "2022-03-20",
             purchaseTime: "99:99",
+            items: [
+                {
+                    shortDescription: "Gatorade",
+                    price: "2.25",
+                },
+            ],
+            total: "9.00",
+        };
+        expect(receiptUtil.validateReceipt(receipt)).toBe(false);
+    });
+
+    test("Receipt with invalid time format should return false", () => {
+        const receipt = {
+            retailer: "CVS",
+            purchaseDate: "2022-03-20",
+            purchaseTime: "12:22AM",
             items: [
                 {
                     shortDescription: "Gatorade",
